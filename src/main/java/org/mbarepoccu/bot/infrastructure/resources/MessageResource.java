18c85d95a37188424aa7d874e502ea4693c7d3f4
Antonino Cortese <antonino.cortese@lastminute.com>
2018-03-31 15:48:17 +0200

    First commit

---
+++ src/main/java/org/mbarepoccu/bot/infrastructure/resources/MessageResource.java
@@ -0,0 +1,21 @@
+package org.mbarepoccu.bot.infrastructure.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class MessageResource
+{
+  private static final Logger LOGGER = LoggerFactory.getLogger(MessageResource.class);
+
+  @RequestMapping(value = "/new-message", method = RequestMethod.POST)
+  public void handle(@RequestBody Map<String, Object> body) {
+    LOGGER.info("Message Received with body {}!", body);
+  }
+}
---
+++ src/test/java/org/mbarepoccu/bot/infrastructure/resources/MessageResourceTest.java
@@ -0,0 +1,13 @@
+package org.mbarepoccu.bot.infrastructure.resources;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MessageResourceTest
+{
+  @Test
+  void true_is_true()
+  {
+    Assertions.assertTrue(true);
+  }
+}
