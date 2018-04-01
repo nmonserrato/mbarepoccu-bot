84e9bdce09f41ad7c8a405c3dce356c55bbf6982
Nino Monserrato <nino.monserrato@gmail.com>
2018-04-01 19:41:14 +0200

    tests now test json output

---
+++ src/main/java/org/mbarepoccu/bot/infrastructure/ObjectMapperFactory.java
@@ -0,0 +1,24 @@
+package org.mbarepoccu.bot.infrastructure;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class ObjectMapperFactory
+{
+  public static ObjectMapper forRestResource()
+  {
+    return new ObjectMapper(){{
+      enable(SerializationFeature.INDENT_OUTPUT);
+      enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+      disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+      disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+      disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+      disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+      disable(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS);
+      disable(SerializationFeature.WRITE_NULL_MAP_VALUES);
+      setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }};
+  }
+}
