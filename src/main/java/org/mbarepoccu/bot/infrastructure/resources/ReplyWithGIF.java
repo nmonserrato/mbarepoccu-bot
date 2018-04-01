753112d2d6bcc9fd118ee9af9f89b7fd90bef417
Nino Monserrato <nino.monserrato@gmail.com>
2018-04-01 21:06:57 +0200

    Revert "ReplyWithGIF is now complete!"
    This reverts commit 1dc4bd5eadc30294d2d729f93bb7c8d056544294.

--- src/main/java/org/mbarepoccu/bot/infrastructure/resources/ReplyWithGIF.java
+++ src/main/java/org/mbarepoccu/bot/infrastructure/resources/ReplyWithGIF.java
@@ -1,21 +1,10 @@
 package org.mbarepoccu.bot.infrastructure.resources;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
 public class ReplyWithGIF extends Reply
 {
-  private final String document;
-
-  public ReplyWithGIF(String chatId, String fileId)
-  {
-    super(chatId, "sendDocument");
-    this.document = fileId;
-  }
-
   @Override
-  public String toString()
+  public String getMethod()
   {
-    return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    return "sendDocument";
   }
 }
