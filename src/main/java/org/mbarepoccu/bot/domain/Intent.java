6c27b54668393e6573efbdc411253de3c5c14fa0
Nino Monserrato <nino.monserrato@gmail.com>
2018-04-02 10:46:57 +0200

    refactoring to intent completed

--- src/main/java/org/mbarepoccu/bot/domain/Intent.java
+++ src/main/java/org/mbarepoccu/bot/domain/Intent.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.mbarepoccu.bot.infrastructure.resources.Reply;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -13,14 +14,163 @@ import static org.mbarepoccu.bot.infrastructure.resources.Reply.Builder.aReply;
 public enum Intent
 {
   WE {
+      @Override
+      public boolean canHandle(Message message)
+      {
+        return StringUtils.equalsIgnoreCase(message.text, "we");
+      }
+
+      @Override
+      public Reply buildAnswer(Message message)
+      {
+        return buildReplyWithRandomText(message, "we dica", "we", "we mbare");
+      }
+  },
+
+  AUNNI_SI {
     @Override
-    public boolean canHandle(Message message) {
-      return StringUtils.equalsIgnoreCase(message.text, "we");
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.containsIgnoreCase(message.text, "aunni si") ||
+        StringUtils.containsIgnoreCase(message.text, "aunni su");
     }
 
     @Override
-    public Reply buildAnswer(Message message) {
-      return buildReplyWithRandomText(message, "we dica", "we", "we mbare");
+    public Reply buildAnswer(Message message)
+    {
+      return buildReplyWithRandomText(message, "casa tu", "casa as usual...io");
+    }
+  },
+  PICCIONE {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.containsIgnoreCase(message.text, "piccione");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return aReply().in(message.chat).withText("non parlo di piccione con te mbare").build();
+    }
+  },
+  CON {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.containsIgnoreCase(message.text, "con chi") ||
+        StringUtils.equalsIgnoreCase(message.text, "con") ||
+        StringUtils.equalsIgnoreCase(message.text, "con?");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return aReply().in(message.chat).withText("tua sorella").build();
+    }
+  },
+  AUNNI_VAI {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.containsIgnoreCase(message.text, "aunni vai");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return buildReplyWithRandomText(message, "eh sapessi mbare", "minchia se potessi parlare mbare", "sapessi");
+    }
+  },
+  FIFA {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.containsIgnoreCase(message.text, "fifa");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return buildReplyWithRandomText(message, "non mi va mbare. EA sports merda!", "naaaah fifa merda");
+    }
+  },
+  NEWS {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.containsIgnoreCase(message.text, "news");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return buildReplyWithRandomText(message, "nada tu");
+    }
+  },
+  WHY {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.containsIgnoreCase(message.text, "why");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return buildReplyWithRandomText(message, "eh sapessi mbare", "potessi parlare...");
+    }
+  },
+  VALENCIA {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.containsIgnoreCase(message.text, "valencia");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return buildReplyWithRandomText(message, "onore a te mbare cheers", "minchia valencia mbare...sei troppo superiore", "non sono degno di parlare di piccione con te mbare");
+    }
+  },
+  TEST_GIF {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.equalsIgnoreCase(message.text, "test gif");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return aReply().in(message.chat).withGIF("CgADBAADE58AAsgeZAc8eTz0lPWp0gI").build();
+    }
+  },
+  TEST_STICKER {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return StringUtils.equalsIgnoreCase(message.text, "test sticker");
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return aReply().in(message.chat).withSticker("CAADBQADuQEAAukKyAMFe9CCAAH2HHcC").build();
+    }
+  },
+  RANDOM {
+    @Override
+    public boolean canHandle(Message message)
+    {
+      return RandomUtils.nextInt(0, 10) < 3;
+    }
+
+    @Override
+    public Reply buildAnswer(Message message)
+    {
+      return buildReplyWithRandomImage(message);
     }
   };
 
@@ -35,5 +185,15 @@ public enum Intent
   private static Reply random(List<Reply> randomResponses)
   {
     return randomResponses.get(RandomUtils.nextInt(0, randomResponses.size()));
+  }
+
+  private static Reply buildReplyWithRandomImage(Message message)
+  {
+    final List<Reply> options = Arrays.asList(
+      aReply().in(message.chat).withSticker("CAADBQADuQEAAukKyAMFe9CCAAH2HHcC").build(),
+      aReply().in(message.chat).withSticker("CAADBAADgAIAAo-zWQNa5qKVuK6KiQI").build(),
+      aReply().in(message.chat).withGIF("CgADBAADE58AAsgeZAc8eTz0lPWp0gI").build()
+    );
+    return random(options);
   }
 }
--- src/main/java/org/mbarepoccu/bot/infrastructure/resources/MessageResource.java
+++ src/main/java/org/mbarepoccu/bot/infrastructure/resources/MessageResource.java
@@ -2,7 +2,6 @@ package org.mbarepoccu.bot.infrastructure.resources;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.RandomUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.mbarepoccu.bot.domain.Intent;
 import org.mbarepoccu.bot.domain.Message;
@@ -16,12 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
-import static org.mbarepoccu.bot.infrastructure.resources.Reply.Builder.aReply;
 
 @RestController
 public class MessageResource
@@ -54,7 +48,12 @@ public class MessageResource
     try
     {
       final Update update = objectMapper.readValue(string, Update.class);
-      final Reply reply = handle(update.message);
+      final Message message = update.message;
+      if (message == null)
+        return null;
+
+      Reply reply = Stream.of(Intent.values()).filter(i -> i.canHandle(message)).findAny().map(i -> i.buildAnswer(message)).orElse(null);
+      LOGGER.info("Replying with {}", ToStringBuilder.reflectionToString(reply));
       sleep();
       return reply;
     }
@@ -63,18 +62,6 @@ public class MessageResource
       LOGGER.error("Error parsing input update", e);
       return null;
     }
-  }
-
-  private Reply handle(Message message) {
-    Reply reply = null;
-
-    if(message != null)
-     reply = buildReplyForMessage(message);
-
-    if(reply != null)
-      LOGGER.info("Replying with {}", ToStringBuilder.reflectionToString(reply));
-
-    return reply;
   }
 
   private void sleep()
@@ -87,90 +74,6 @@ public class MessageResource
       catch (InterruptedException e) {
       }
     }
-  }
-
-  private Reply buildReplyForMessage(Message message)
-  {
-    final Reply reply = Stream.of(Intent.values())
-      .filter(i -> i.canHandle(message))
-      .findAny()
-      .map(i -> i.buildAnswer(message))
-      .orElse(null);
-
-    if (reply != null)
-    {
-      return reply;
-    }
-
-    if (StringUtils.containsIgnoreCase(message.text, "aunni si") ||
-        StringUtils.containsIgnoreCase(message.text, "aunni su")){
-      return buildReplyWithRandomText(message, "casa tu", "casa as usual...io");
-    }
-
-    if (StringUtils.containsIgnoreCase(message.text, "piccione")){
-      return aReply().in(message.chat).withText("non parlo di piccione con te mbare").build();
-    }
-
-    if (StringUtils.containsIgnoreCase(message.text, "con chi") ||
-        StringUtils.equalsIgnoreCase(message.text, "con") ||
-      StringUtils.equalsIgnoreCase(message.text, "con?")){
-      return aReply().in(message.chat).withText("tua sorella").build();
-    }
-
-    if (StringUtils.containsIgnoreCase(message.text, "aunni vai")){
-      return buildReplyWithRandomText(message, "eh sapessi mbare", "minchia se potessi parlare mbare", "sapessi");
-    }
-
-    if (StringUtils.containsIgnoreCase(message.text, "fifa")){
-      return buildReplyWithRandomText(message, "non mi va mbare. EA sports merda!", "naaaah fifa merda");
-    }
-
-    if (StringUtils.containsIgnoreCase(message.text, "news")){
-      return buildReplyWithRandomText(message, "nada tu");
-    }
-
-    if (StringUtils.containsIgnoreCase(message.text, "why")){
-      return buildReplyWithRandomText(message, "eh sapessi mbare", "potessi parlare...");
-    }
-
-    if (StringUtils.containsIgnoreCase(message.text, "valencia")){
-      return buildReplyWithRandomText(message, "onore a te mbare cheers", "minchia valencia mbare...sei troppo superiore", "non sono degno di parlare di piccione con te mbare");
-    }
-
-    if (StringUtils.equalsIgnoreCase(message.text, "test gif")){
-      return aReply().in(message.chat).withGIF("CgADBAADE58AAsgeZAc8eTz0lPWp0gI").build();
-    }
-
-    if (StringUtils.equalsIgnoreCase(message.text, "test sticker")){
-      return aReply().in(message.chat).withSticker("CAADBQADuQEAAukKyAMFe9CCAAH2HHcC").build();
-    }
-
-    if (RandomUtils.nextInt(0, 10) < 2)
-    {
-      return buildReplyWithRandomImage(message);
-    }
-
-    return null;
-  }
-
-  private Reply buildReplyWithRandomImage(Message message)
-  {
-    final List<Reply> options = Arrays.asList(
-      aReply().in(message.chat).withSticker("CAADBQADuQEAAukKyAMFe9CCAAH2HHcC").build(),
-      aReply().in(message.chat).withSticker("CAADBAADgAIAAo-zWQNa5qKVuK6KiQI").build(),
-      aReply().in(message.chat).withGIF("CgADBAADE58AAsgeZAc8eTz0lPWp0gI").build()
-    );
-    return random(options);
-  }
-
-  private Reply buildReplyWithRandomText(Message message, String... textuals)
-  {
-    return random(Stream.of(textuals).map(t -> aReply().in(message.chat).withText(t).build()).collect(toList()));
-  }
-
-  private Reply random(List<Reply> randomResponses)
-  {
-    return randomResponses.get(RandomUtils.nextInt(0, randomResponses.size()));
   }
 }
 
